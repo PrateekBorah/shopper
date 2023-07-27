19d15526206e1fcea876568d7e02aa72c8e92839
Prateek Borah <prateekborah1909@gmail.com>
2023-07-27 18:48:09 +0530

    fixed bugs

--- solar/src/components/Footer/Footer.jsx
+++ solar/src/components/Footer/Footer.jsx
@@ -50,8 +50,8 @@ export default function Footer() {
           viewBox="0 0 24 24">
           <path
             d="M7 11v2.4h3.97c-.16 1.029-1.2 3.02-3.97 3.02-2.39 0-4.34-1.979-4.34-4.42 0-2.44 1.95-4.42 4.34-4.42 1.36 0 2.27.58 2.79 1.08l1.9-1.83c-1.22-1.14-2.8-1.83-4.69-1.83-3.87 0-7 3.13-7 7s3.13 7 7 7c4.04 0 6.721-2.84 6.721-6.84 0-.46-.051-.81-.111-1.16h-6.61zm0 0 17 2h-3v3h-2v-3h-3v-2h3v-3h2v3h3v2z"
-            fill-rule="evenodd"
-            clip-rule="evenodd" />
+            fillRule="evenodd"
+            clipRule="evenodd" />
         </svg>
       </a>
 
@@ -121,7 +121,7 @@ export default function Footer() {
               id="exampleFormControlInput1"
               placeholder="Email address" />
             <label
-              for="exampleFormControlInput1"
+              htmlFor="exampleFormControlInput1"
               className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-200 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-neutral-200 peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-neutral-200"
               >Email address
             </label>
